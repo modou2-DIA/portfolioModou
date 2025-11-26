bd92eb0d6820d0655fddd099cce6fbd684367754
modou2-DIA <101325666+modou2-DIA@users.noreply.github.com>
2025-11-26 02:11:21 +0100

    Initial commit - Dépôt nettoyé et optimisé 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
 
 import { ClientLayout } from "@/components/client-layout"
 
