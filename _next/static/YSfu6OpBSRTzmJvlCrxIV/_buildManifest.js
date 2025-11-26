3a2efd4a2a2c47171cd25aa8590b2d7baba7a783
modou2-DIA <101325666+modou2-DIA@users.noreply.github.com>
2025-11-26 02:58:26 +0100

    Updates

--- _next/static/YSfu6OpBSRTzmJvlCrxIV/_buildManifest.js
+++ _next/static/YSfu6OpBSRTzmJvlCrxIV/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/portfolioModou//_next/:path+",
-        "destination": "/portfolioModou/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
